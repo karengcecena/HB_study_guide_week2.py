0d39ca4e3ecaa528776e46cbab43a63b7e7439fc
karengcecena <karengcecena@gmail.com>
2022-09-29 22:33:57 -0700

    Working pt 3- Automates Exam Administration

--- oo.py
+++ oo.py
@@ -17,6 +17,7 @@ class Question():
         self.answer = answer
 
     def ask_and_evaluate(self):
+        """Asks question and returns if answer given matches correct answer"""
         
         given_answer = input(f"{self.question}> ")
         
@@ -36,7 +37,9 @@ class Exam():
         Exam.questions.append(question_to_add)
 
 
-    def administer(self):
+    def administer():
+        """ Iterates through question list and counts correct answers """
+
         count = 0
 
         for question in Exam.questions:
@@ -48,32 +51,58 @@ class Exam():
 
 
 class StudentExam():
+    
 
-    def __init__():
-        """Initializes everything"""
+    def __init__(self, student, exam, student_score):
+        """Stores student information, exam type, and student score for that exam"""
+
+        self.Student = student
+        self.exam_name = exam
+        self.student_score = student_score
+        
 
     def take_test():
         """Administers exam and assigns score to StudentExam instance"""
-        # print score
+
+        student_score = Exam.administer()
+        print(f"Score: {student_score}") 
+
 
 
 def example():
-    """
-    Creates an exam
+   
+    # Creates an exam:
+    exam = Exam("Midterm")
 
-    Adds a few questions to the exam
+    # Defines a few questions for the exam:
+    list_q = Question('Python lists are mutable, iterable, and what?','ordered')
+    set_q = Question('What is the method for adding an element to a set?', '.add()')
+    pwd_q = Question('What does pwd stand for?','print working directory')
 
-    These should be part of the function; no need to prompt the user for questions.
+    # Adds a few questions to the exam:
+    exam.add_question(set_q)
+    exam.add_question(pwd_q)
+    exam.add_question(list_q)
 
-    Creates a student
 
-    Instantiates a StudentExam, passing the student and exam you just created as arguments
+    # Creates a student:
+    student_1 = Student("Jasmine", "Debugger", "0101 Computer Street")
+    # student_2 = Student("Jacqui", "Console", "888 Binary Ave")
 
-    Administers the test for that student using the take_test method you wrote
-    """
+
+    # Administers the test for that student using the take_test method you wrote:
+    score = StudentExam.take_test()
+
+    # Instantiates a StudentExam, passing the student and exam you just created as arguments:
+    student_and_exam = StudentExam(student_1, exam, score)
+
+    
+   
+example()
 
     # Part 4:
-    """
+   
+"""
     When you call the administer method on a quiz, it should return 1 if you passed or 0 if you failed.
     Think about how you could solve this requirement: you have an Exam class and you want to have a Quiz class that is similar.
 
